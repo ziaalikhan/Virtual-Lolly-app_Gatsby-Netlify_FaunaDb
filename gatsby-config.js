7a28254b23c1c5d9251d436afd9e0b756e860bde
zia <ziaalikhan052@gmail.com>
2021-11-11 21:50:05 -0800

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,4 @@ module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`,
     },
-    plugins: [
-        {
-            resolve: "gatsby-source-graphql",
-            options: {        
-              typeName: "GSG",        
-              fieldName: "GetLollies",       
-              url: "https://vriual-lolly-gatsby.netlify.app/.netlify/functions/LollyApp",
-            },
-          }    
-    ]
 }
